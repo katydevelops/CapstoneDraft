dfcb15a9da5bd65110e7fede9dbf9659377d4a0d
Katy <ksenia1@wgu.edu>
2024-03-16 19:49:37 -0400

    // Creating custom password annotation to meet stadard security requirement for assignment

--- DataAnnotations/PasswordAnnotation.cs
+++ DataAnnotations/PasswordAnnotation.cs
@@ -4,5 +4,21 @@ namespace CapstoneDraft.DataAnnotations
 {
     public class PasswordAnnotation : ValidationAttribute // Example of inheritance for assignment requirements
     {
+        public override bool IsValid(object value)
+        {
+            // Creating custom password annotation to meet stadard security requirement for assignment
+            var validPassword = value as string;
+            if (validPassword.Length < 10)
+            {
+                ErrorMessage = "Your password must be at least 10 characters long!";
+                return false;
+            }
+            if(!validPassword.Any(char.IsDigit))
+            {
+                ErrorMessage = "Your password must contain at least one number!";
+                return false;
+            }
+            return true;
+        }
     }
 }
