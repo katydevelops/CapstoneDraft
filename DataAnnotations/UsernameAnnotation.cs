c1ceed838d31aa7dc2a0a1f9a0f5dd2ccf82e1c9
Katy <ksenia1@wgu.edu>
2024-03-16 19:23:46 -0400

    ErrorMessage = "Your username must be between 7 and 14 characters long!";

--- DataAnnotations/UsernameAnnotation.cs
+++ DataAnnotations/UsernameAnnotation.cs
@@ -1,6 +1,20 @@
-﻿namespace CapstoneDraft.DataAnnotations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapstoneDraft.DataAnnotations
 {
-    public class UsernameAnnotation
+    public class UsernameAnnotation : ValidationAttribute
     {
+        public override bool ValidUsername(object value)
+        {
+            var validatedUsername = value as string;
+            if (string.IsNullOrEmpty(validatedUsername))
+            {
+                ErrorMessage = "Please enter a username!";
+            }
+            if (validatedUsername.Length < 7 || validatedUsername.Length > 14)
+            {
+                ErrorMessage = "Your username must be between 7 and 14 characters long!";
+            }
+        }
     }
 }
--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,8 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CapstoneDraft.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace CapstoneDraft.Models
 {
     public class RegisterModel
     {
+        [Required]
+        public string FirstName { get; set; }
+        [Required] 
+        public string LastName { get; set; }
+
+        // Using customer DataAnnotations for Username and Password format requirements
+        [Required]
+        [UsernameAnnotation]
+        public string Username { get; set; }
+
+
+
+        // Using ASP.NET built in DataAnnotations for phone and email format
     }
 }
