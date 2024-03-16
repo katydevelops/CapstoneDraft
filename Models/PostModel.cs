fd9c160f4330c15df7241f3fff0d15e47b41f2fa
Katy <ksenia1@wgu.edu>
2024-03-16 18:23:00 -0400

    public DateTime PostCreatedTimestamp { get; set; } = DateTime.UtcNow;

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -3,6 +3,15 @@
     public class PostModel
     {
         public int PostId { get; set; }
+        public string PostUserId { get; set; }
+        public string PostCategory { get; set; }
+        public string NameOfPoster { get; set; }
+        public string UserLocation { get; set; }
+        public string PostSubject { get; set; }
+        public string PostBody { get; set; }
+        public string? PostPhoto { get; set; }
+        public DateTime PostCreatedTimestamp { get; set; } = DateTime.UtcNow;
+
 
     }
 }
