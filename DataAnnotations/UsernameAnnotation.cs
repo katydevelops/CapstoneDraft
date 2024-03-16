6ee95e5e21d8370c2b2e5c4a4bf45d73fed0d69d
Katy <ksenia1@wgu.edu>
2024-03-16 19:43:03 -0400

    // Overriding the base IsValid class is an example that allows for polymorphism of the username object when it's called throughout the app

--- DataAnnotations/UsernameAnnotation.cs
+++ DataAnnotations/UsernameAnnotation.cs
@@ -2,11 +2,12 @@
 
 namespace CapstoneDraft.DataAnnotations
 {
-    public class UsernameAnnotation : ValidationAttribute
+    public class UsernameAnnotation : ValidationAttribute // Example of inheritance for assignment requirements
     {
-        // Custom username annotation created to meet standard security requirements for this assignment
+        // Overriding the base IsValid class is an example that allows for polymorphism of the username object when it's called throughout the app 
         public override bool IsValid(object value)
         {
+            // Custom username annotation created to meet standard security requirements for this assignment
             var validatedUsername = value as string;
             if (validatedUsername.Length < 7 || validatedUsername.Length > 14)
             {
