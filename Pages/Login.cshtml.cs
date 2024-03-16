4b291c224012e04c66b12b5cd3396feecdaf5235
Katy <ksenia1@wgu.edu>
2024-03-16 15:55:58 -0400

    [BindProperty]

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -10,10 +10,16 @@ namespace CapstoneDraft.Pages
     {
         private readonly SignInManager<UserModel> _loginManager;
 
-
+        // Safety Net utilizes ASP.NET Identity for authentication and authorization
+        // Constructor to load SignInManager
         public LoginModel(SignInManager<UserModel> loginManager)
         {
             _loginManager = loginManager;
         }
+
+        // BindProperty will capture data from input fields that the user enters during login
+        [BindProperty]
+        public string Username { get; set; }
+
     }
 }
