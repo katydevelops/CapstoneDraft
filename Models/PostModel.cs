c85b21bbba78b732702d7696dd166b0293a96566
Katy <ksenia1@wgu.edu>
2024-03-16 18:26:25 -0400

    public virtual ICollection<CommentModel> PostComments { get; set; } = new List<CommentModel>();

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -5,13 +5,14 @@
         public int PostId { get; set; }
         public string PostUserId { get; set; }
         public string PostCategory { get; set; }
-        public string NameOfPoster { get; set; }
+        public string NameOfPostAuthor { get; set; }
         public string UserLocation { get; set; }
         public string PostSubject { get; set; }
         public string PostBody { get; set; }
         public string? PostPhoto { get; set; }
         public DateTime PostCreatedTimestamp { get; set; } = DateTime.UtcNow;
-
-
+        public int PostLikes { get; set; } = 0; // Initialize post likes to zero
+        public virtual UserModel PostAuthor { get; set; }
+        public virtual ICollection<CommentModel> PostComments { get; set; } = new List<CommentModel>();
     }
 }
