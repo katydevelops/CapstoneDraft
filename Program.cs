dfb98d61384d70eef6da00f9b12c7d79bb049636
Katy <ksenia1@wgu.edu>
2024-03-16 10:07:56 -0400

    // Services that will be used in Safety Net app

--- Program.cs
+++ Program.cs
@@ -4,17 +4,16 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+// Services that will be used in Safety Net app
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
