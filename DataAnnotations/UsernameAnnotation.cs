c96601658c2d04f9d6cf4e47973e97df1c4d3769
Katy <ksenia1@wgu.edu>
2024-03-16 19:38:07 -0400

    // Custom username annotation created to meet standard security requirements for this assignment

--- DataAnnotations/UsernameAnnotation.cs
+++ DataAnnotations/UsernameAnnotation.cs
@@ -4,17 +4,16 @@ namespace CapstoneDraft.DataAnnotations
 {
     public class UsernameAnnotation : ValidationAttribute
     {
-        public override bool ValidUsername(object value)
+        // Custom username annotation created to meet standard security requirements for this assignment
+        public override bool IsValid(object value)
         {
             var validatedUsername = value as string;
-            if (string.IsNullOrEmpty(validatedUsername))
-            {
-                ErrorMessage = "Please enter a username!";
-            }
             if (validatedUsername.Length < 7 || validatedUsername.Length > 14)
             {
                 ErrorMessage = "Your username must be between 7 and 14 characters long!";
+                return false;
             }
+            return true;
         }
     }
 }
