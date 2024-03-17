dc0a3c656466a443ee0a936dd7f368cb1dab89ca
Katy <ksenia1@wgu.edu>
2024-03-16 20:58:08 -0400

    public int PostId { get; set; } // Links comment to the affiliated post

--- Models/CommentModel.cs
+++ Models/CommentModel.cs
@@ -2,5 +2,8 @@
 {
     public class CommentModel
     {
+        public int CommentId { get; set; }
+        public int CommentUserId { get; set; } // Links the comment to the correct user who posted it
+        public int PostId { get; set; } // Links comment to the affiliated post
     }
 }
