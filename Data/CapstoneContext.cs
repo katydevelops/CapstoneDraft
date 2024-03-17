d16aef70291e920be6028d778eb06997d82f27db
Katy <ksenia1@wgu.edu>
2024-03-16 21:54:57 -0400

    using CapstoneDraft.Models;

--- Data/CapstoneContext.cs
+++ Data/CapstoneContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CapstoneDraft.Models;
 
 namespace CapstoneDraft.Data
 {
     // Safety Net uses SQLite with EntityFramework with ASP.NET's Identity API due to the extensive built-in methods and functionality 
     public class  CapstoneContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        public static async Task Initialize(CapstoneContext databaseConnection, UserManager<User> userManager)
     }
 }
