413c8b85d71ea2bac792e1d34f0c800cf6f4ee0d
Katy <ksenia1@wgu.edu>
2024-03-16 20:22:37 -0400

    public IBrowserFile TempPostPhotoFilePath { get; set; } // This will be needed for image compression to prevent users from uploaded large files

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -10,13 +10,23 @@ namespace CapstoneDraft.Models
         [Required] 
         public string LastName { get; set; }
 
-        // Using customer DataAnnotations for Username and Password format requirements
+        // Using custom DataAnnotations for Username and Password format requirements
         [Required]
         [UsernameAnnotation]
         public string Username { get; set; }
 
+        [Required]
+        [PasswordAnnotation] 
+        public string Password { get; set; }
 
 
         // Using ASP.NET built in DataAnnotations for phone and email format
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
--- Models/TemporaryPostModel.cs
+++ Models/TemporaryPostModel.cs
@@ -1,6 +1,15 @@
-﻿namespace CapstoneDraft.Models
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace CapstoneDraft.Models
 {
+    // An additional model was needed to hold the input from the user's post before it was stored permanently in the PostModel
     public class TemporaryPostModel
     {
+        public string TempPostCategory { get; set; }
+        public string TempPostNameOfPostAuthor { get; set;}
+        public string TempPostSubject { get; set; }
+        public string TempPostMessageBody { get; set; }
+        public string TempPostPhoto {  get; set; }
+        public IBrowserFile TempPostPhotoFilePath { get; set; } // This will be needed for image compression to prevent users from uploaded large files
     }
 }
