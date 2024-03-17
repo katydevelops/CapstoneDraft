296e0802933d4416c0c3a5b98dd1d46bf99bba68
Katy <ksenia1@wgu.edu>
2024-03-16 22:03:47 -0400

    public DbSet<CommentModel> Comments { get; set; }

--- Data/CapstoneContext.cs
+++ Data/CapstoneContext.cs
@@ -8,9 +8,10 @@ namespace CapstoneDraft.Data
     // Safety Net uses SQLite with EntityFramework with ASP.NET's Identity API due to the extensive built-in methods and functionality 
     public class  CapstoneContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
-        // Register the database tables based on the Safety Net models
+        // Register the Entity Framework database tables based on the Safety Net User, Post and Comment models
         public DbSet<UserModel> Users { get; set; }
         public DbSet<PostModel> Posts { get; set; }
+        public DbSet<CommentModel> Comments { get; set; }
         public static async Task Initialize(CapstoneContext databaseConnection, UserManager<UserModel> userManager)
         {
 
