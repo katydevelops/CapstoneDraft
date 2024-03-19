1f9443376b924dacb0c9df57f402fc885ee5c557
Katy <ksenia1@wgu.edu>
2024-03-19 06:25:32 -0400

    public string TempPostUserLocation { get; set; }

--- Models/TemporaryPostModel.cs
+++ Models/TemporaryPostModel.cs
@@ -7,6 +7,7 @@ namespace CapstoneDraft.Models
     {
         public string TempPostCategory { get; set; }
         public string TempPostNameOfPostAuthor { get; set;}
+        public string TempPostUserLocation { get; set; }
         public string TempPostSubject { get; set; }
         public string TempPostMessageBody { get; set; }
         public string TempPostPhoto {  get; set; }
