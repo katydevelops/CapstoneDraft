9a67b06075340b3ff704a9c99128f0203a7ef15b
Katy <ksenia1@wgu.edu>
2024-03-21 21:39:22 -0400

    PostPhoto

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -17,7 +17,7 @@ namespace CapstoneDraft.Models
         public string PostSubject { get; set; }
         [Required]
         public string PostMessageBody { get; set; }
-        public string? PostPhoto { get; set; } // Allow photo to be nullable incase user doesn't want to post a photo
+        public string? PostPhoto { get; set; } // Allow photo to be nullable in case user doesn't want to post a photo
         public DateTime PostCreatedTimestamp { get; set; } = DateTime.UtcNow;
         public int PostLikes { get; set; } = 0; // Initialize post likes to zero
         public virtual UserModel User { get; set; }
