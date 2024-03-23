28b09b7b7e8ae8b21c9c9e20837b316bfaaedb68
Katy <ksenia1@wgu.edu>
2024-03-23 00:20:05 -0400

    GetTotalPostsPerUserAsync

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -1,6 +1,21 @@
-﻿namespace CapstoneDraft.Services
+﻿using CapstoneDraft.Data;
+
+namespace CapstoneDraft.Services
 {
     public class ReportService
     {
+        private readonly CapstoneContext _databaseConnection;
+
+        public ReportService(CapstoneContext databaseConnection)
+        {
+            _databaseConnection = databaseConnection;
+        }
+
+        public async Task<Dictionary<string, (int PostCount, DateTime? LatestPostDate)>> GetTotalPostsPerUserAsync()
+        {
+
+        }
     }
+
+
 }
