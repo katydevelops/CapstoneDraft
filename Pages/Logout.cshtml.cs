bc1822877c1381cc313a065605c831949ae49c95
Katy <ksenia1@wgu.edu>
2024-03-28 19:48:19 -0400

    namespace CapstoneDraft.Pages

--- Pages/Logout.cshtml.cs
+++ Pages/Logout.cshtml.cs
@@ -1,12 +1,30 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CapstoneDraft.Pages
 {
+    [AllowAnonymous] // Allow access without requiring login
     public class LogoutModel : PageModel
     {
-        public void OnGet()
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager)
         {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string command)
+        {
+            if (command == "logout")
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToPage("/Login");
+            }
+
+            return RedirectToPage("/");
         }
     }
 }
