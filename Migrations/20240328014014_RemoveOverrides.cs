6b4bae8ff07acbe76044fe9d00f461cc79547e7f
Katy <ksenia1@wgu.edu>
2024-03-27 21:46:19 -0400

    update model

---
+++ Migrations/20240328014014_RemoveOverrides.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapstoneDraft.Migrations
+{
+    public partial class RemoveOverrides : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
