bcd69e4a47347bf01144d81e34e6beef8eeab208
Katy <ksenia1@wgu.edu>
2024-03-27 20:12:03 -0400

    EmailAddress

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -9,6 +9,11 @@ namespace CapstoneDraft.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        [EmailAddress]
+        public override string Email { get; set; }
+
+        [Phone]
+        public override string PhoneNumber { get; set; }
 
         public DateTime CreatedTimeStamp { get; set; }
         public DateTime? UserLastActiveTimeStamp { get; set; }
