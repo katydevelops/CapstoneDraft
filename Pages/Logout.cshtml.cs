f6737ed9e7a02171cda39d93a184b7271c0200a0
Katy <ksenia1@wgu.edu>
2024-03-30 22:38:11 -0400

    .validation-summary {

--- Pages/Logout.cshtml.cs
+++ Pages/Logout.cshtml.cs
@@ -5,10 +5,11 @@ using CapstoneDraft.Models;
 
 namespace CapstoneDraft.Pages
 {
-    public class LogoutModel : PageModel
+    public class LogoutModel : PageModel // Example of inheritance which is required in this assignment
     {
         private readonly SignInManager<UserModel> _signOutManager;
 
+        // Constructor to initialize the SignInManager package of ASP.Net Identity which will be used in this page specifically to log the user out of the application
         public LogoutModel(SignInManager<UserModel> signOutManager)
         {
             _signOutManager = signOutManager;
