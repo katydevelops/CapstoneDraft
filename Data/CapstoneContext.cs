27828d49edea3ce4c26bb830492802dbf4e2f699
Katy <ksenia1@wgu.edu>
2024-03-31 12:48:08 -0400

    ReportService

--- Data/CapstoneContext.cs
+++ Data/CapstoneContext.cs
@@ -15,5 +15,14 @@ namespace CapstoneDraft.Data
 
         // Needed to include constructor that accepts DbContextOptions in order to properly register SQLite as underlying EF database that will be used in Safety Net
         public CapstoneContext(DbContextOptions<CapstoneContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Override the Entity Framework ModelBuilder to ensure that the email and username stay unique throughout the application
+            builder.Entity<UserModel>().HasIndex(unique => unique.Email).IsUnique();
+            builder.Entity<UserModel>().HasIndex(unique => unique.UserName).IsUnique();
+        }
     }
 }
---
+++ Migrations/20240331041651_AddUniqueEmailUsernameIndex.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapstoneDraft.Migrations
+{
+    public partial class AddUniqueEmailUsernameIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserName",
+                table: "AspNetUsers",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserName",
+                table: "AspNetUsers");
+        }
+    }
+}
