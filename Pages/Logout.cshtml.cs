7177ac0adf2d0df83530af6c87db675f6c567572
Katy <ksenia1@wgu.edu>
2024-03-30 22:30:03 -0400

    signOutManager

--- Pages/Logout.cshtml.cs
+++ Pages/Logout.cshtml.cs
@@ -2,44 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Identity;
 using CapstoneDraft.Models;
-using Microsoft.Extensions.Logging;
 
 namespace CapstoneDraft.Pages
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<UserModel> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+        private readonly SignInManager<UserModel> _signOutManager;
 
-
-        public LogoutModel(SignInManager<UserModel> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<UserModel> signOutManager)
         {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
-
-        // This method is called when navigating to /Logout, showing the confirmation message
-        public void OnGet()
-        {
-            // Log when the logout page is accessed
-            _logger.LogInformation("Accessed Logout page");
+            _signOutManager = signOutManager;
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            _logger.LogInformation("Onpost is being trigger right now: ");
-            try
-            {
-                await _signInManager.SignOutAsync();
-                _logger.LogInformation("User logged out successfully.");
+            // The form post from the Logout razor page will trigger the built-in Sign Out Async method which will handling the sign out process and redirect the user to the login page
+                await _signOutManager.SignOutAsync();
                 return LocalRedirect("/");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "An error occurred while logging out.");
-                // Optionally redirect to an error page or return to the current page with an error message
-                return RedirectToPage("/Error");
-            }
         }
     }
 }
