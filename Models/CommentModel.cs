a971b0200de433107f98a5a6b1c886a41bada34c
Katy <ksenia1@wgu.edu>
2024-04-03 19:19:53 -0400

    public PostModel Post { get; set; }

--- Models/CommentModel.cs
+++ Models/CommentModel.cs
@@ -11,6 +11,8 @@ namespace CapstoneDraft.Models
         public int PostId { get; set; } // Links comment to the affiliated post
         public string CommentText { get; set; }
         public DateTime CommentCreatedTimestamp { get; set; } = DateTime.UtcNow;
+
+        public PostModel Post { get; set; }
         public virtual UserModel User { get; set; }
     }
 }
