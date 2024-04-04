01c5c7a5b7a079f5a7778ebec2f0712590bdb204
Katy <ksenia1@wgu.edu>
2024-04-03 22:10:19 -0400

    CreatedTimeStamp

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -9,7 +9,7 @@ namespace CapstoneDraft.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public DateTime CreatedTimeStamp { get; set; }
+        //public DateTime CreatedTimeStamp { get; set; }
         public DateTime? UserLastActiveTimeStamp { get; set; }
 
         // Create collection data structure to store the user's affiliated posts and comments
@@ -19,7 +19,7 @@ namespace CapstoneDraft.Models
         // Store the user creation times in UTC in the database
         public UserModel ()
         {
-            CreatedTimeStamp = DateTime.UtcNow;
+            //CreatedTimeStamp = DateTime.UtcNow;
             UserLastActiveTimeStamp = DateTime.UtcNow;
         }
     }
