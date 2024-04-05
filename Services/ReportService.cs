e5a0d8aeb2279c5265e610a36a66cbdf9ab15a0e
Katy <ksenia1@wgu.edu>
2024-04-05 13:08:47 -0400

    Get the user'

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -14,6 +14,7 @@ namespace CapstoneDraft.Services
 
         public async Task<Dictionary<string, (int TotalPosts, DateTime? LatestPostTimestamp)>> GetTotalPostsPerUserAsync()
         {
+            // Receive the total number of posts from the database based on the user object and returns the information as a dictionary where the total post number and latest post timestamp are returned as the values to the user key.
             var totalPostPerUser = await _databaseConnection.Users.Select(user => new
             {
                 Username = user.UserName,
@@ -25,6 +26,7 @@ namespace CapstoneDraft.Services
 
         public async Task<Dictionary<string, (int TotalComments, DateTime? LatestCommentTimestamp)>> GetTotalCommentsPerUserAsync()
         {
+            // Receive the total number of comments from the database based on the user object and returns the information as a dictionary where the total comment number and latest comment timestamp are returned as the values to the user key.
             return await _databaseConnection.Comments.GroupBy(comment => comment.User.UserName)
                 .Select(group => new
                 {
@@ -36,11 +38,13 @@ namespace CapstoneDraft.Services
 
         public async Task<Dictionary<string, int>> GetTotalPostsPerCategoryAsync()
         {
+            // Return the total number count of each type of post category and return as a dictionary where the post category is the key and the count is the value
             return await _databaseConnection.Posts.GroupBy(post => post.PostCategory).Select(group => new { PostCategory = group.Key, Count = group.Count() }).ToDictionaryAsync(group => group.PostCategory, group => group.Count);
         }
 
         public async Task<List<(string UserName, string Email, DateTime? UserLastActive)>> GetUserLastActiveDetailsAsync()
         {
+            // Get the user's username, email and last active timestamp and return these details as a list storted by the user's with the most recent login timestamp to the user's who have not logged in recently.
             var userLastActive = await _databaseConnection.Users
                 .Select(user => new
                 {
