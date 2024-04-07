e4bbd9b166709b70fd07ea7dae3935dd6e04a55b
Katy <ksenia1@wgu.edu>
2024-04-07 18:35:18 -0400

    ValidationSummary

--- Migrations/20240407222710_UpdateRequiredNameUserModel.Designer.cs
+++ Migrations/20240407222710_UpdateRequiredNameUserModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CapstoneDraft.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CapstoneDraft.Migrations
 {
     [DbContext(typeof(CapstoneContext))]
-    partial class CapstoneContextModelSnapshot : ModelSnapshot
+    [Migration("20240407222710_UpdateRequiredNameUserModel")]
+    partial class UpdateRequiredNameUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.28");
---
+++ Migrations/20240407222710_UpdateRequiredNameUserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapstoneDraft.Migrations
+{
+    public partial class UpdateRequiredNameUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedTimeStamp",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedTimeStamp",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
