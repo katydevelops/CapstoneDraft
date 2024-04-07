13f97bb89a3fb3d9afda04e9f8e83b94c762d634
Katy <ksenia1@wgu.edu>
2024-04-07 18:39:22 -0400

    // UNIT TESTING REMIDIATION

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -6,9 +6,11 @@ namespace CapstoneDraft.Models
     // Inheriting from ASP.NET IdentityUser is a convenient way to take avantage of the built-in functionality from the Identity API such as PasswordHash and LockedOutEnable(to meet the security requiremennts of this assignment) as well as automatically preventing duplicate usernames and emails from being stored in the database
     public class UserModel : IdentityUser // Example of inheritance in Safety Net
     {
+        // UNIT TESTING REMIDIATION: Through unit testing, I discovered that users were able to update their user profile after successfully removing their first name. In order to preserve data integrity, I added the [Required] keyword above the First Name property in the UserModel.
         [Required]
         public string FirstName { get; set; }
 
+        // UNIT TESTING REMIDIATION: Through unit testing, I discovered that users were able to update their user profile after successfully removing their last name. In order to preserve data integrity, I added the [Required] keyword above the Last Name property in the UserModel.
         [Required]
         public string LastName { get; set; }
 
